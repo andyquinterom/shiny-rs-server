fb81585389ea65057cd0b42a94897d42c910031c
Andrés Felipe Quintero Moreano <afquinteromoreano@gmail.com>
2022-08-15 04:11:27 -0500

    Add implements new derive

--- src/session.rs
+++ src/session.rs
@@ -8,12 +8,6 @@ use shiny_rs_derive::ShinyHandler;
 pub mod traits;
 use traits::*;
 
-/// How often heartbeat pings are sent
-const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
-
-/// How long before lack of client response causes a timeout
-const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
-
 pub mod input_pool;
 
 pub type ShinyContext<T> = actix_web_actors::ws::WebsocketContext<T>;
@@ -26,51 +20,18 @@ pub struct ShinyServer {
     pub event: String,
     initialize: fn(&mut Self, session: &mut <Self as Actor>::Context),
     update: fn(&mut Self, session: &mut <Self as Actor>::Context),
-    tick: fn(&mut Self, session: &mut <Self as Actor>::Context)
-}
-
-pub trait ShinyLogic {
-    fn input(&mut self) -> &mut input_pool::InputPool;
-    fn get_last_hb(&self) -> Instant;
-    fn hb(&self, session: &mut ws::WebsocketContext<Self>) where Self: actix::Actor<Context = ws::WebsocketContext<Self>> {
-        session.run_interval(HEARTBEAT_INTERVAL, |act, session| {
-            if Instant::now().duration_since(act.get_last_hb()) > CLIENT_TIMEOUT {
-                println!("Shiny client heartbeat fialed, disconnecting!");
-                session.stop();
-                return;
-            }
-            session.ping(b"");
-        });
-    }
-
-    fn check_change(&mut self, msg: &ShinyMsg, key: &str) -> bool {
-        let v: &serde_json::Value = &msg.data[key];
-        let prev_val = self.input().get(key).unwrap_or(&serde_json::Value::Null);
-        if v.is_null() & prev_val.is_null() {
-            return false
-        }
-        if prev_val.is_null() | (v != prev_val) {
-            self.input().insert(&key, v.clone());
-            return true
-        }
-        return false
-    }
-}
-
-impl ShinyLogic for ShinyServer {
-    fn input(&mut self) -> &mut input_pool::InputPool{
-        return &mut self.input;
-    }
-    fn get_last_hb(&self) -> Instant {
-        return self.hb
-    }
+    tick: fn(&mut Self, session: &mut <Self as Actor>::Context),
+    hb_interval: Duration,
+    client_timeout: Duration
 }
 
 impl ShinyServer {
     pub fn new(
         initialize: fn(&mut Self, session: &mut <Self as Actor>::Context),
         update: fn(&mut Self, session: &mut <Self as Actor>::Context),
-        tick: fn(&mut Self, session: &mut <Self as Actor>::Context)
+        tick: fn(&mut Self, session: &mut <Self as Actor>::Context),
+        hb_interval: Duration,
+        client_timeout: Duration
     ) -> Self {
         Self {
             hb: Instant::now(),
@@ -78,7 +39,9 @@ impl ShinyServer {
             event: String::from("init"),
             initialize,
             update,
-            tick
+            tick,
+            hb_interval,
+            client_timeout
         }
     }
 }
--- src/session/traits.rs
+++ src/session/traits.rs
@@ -1,3 +1,39 @@
 pub extern crate actix;
 
 pub use actix::prelude::*;
+use std::time::{Duration, Instant};
+use actix_web_actors::ws;
+
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+
+pub trait ShinyLogic {
+    fn input(&mut self) -> &mut super::input_pool::InputPool;
+    fn get_last_hb(&self) -> Instant;
+    fn get_hb_interval(&self) -> Duration;
+    fn get_client_timeout(&self) -> Duration;
+    fn hb(&self, session: &mut ws::WebsocketContext<Self>) where Self: actix::Actor<Context = ws::WebsocketContext<Self>> {
+        let client_timeout = self.get_client_timeout();
+        session.run_interval(self.get_hb_interval(), move |act, session| {
+            if Instant::now().duration_since(act.get_last_hb()) > client_timeout {
+                println!("Shiny client heartbeat fialed, disconnecting!");
+                session.stop();
+                return;
+            }
+            session.ping(b"");
+        });
+    }
+
+    fn check_change(&mut self, msg: &super::ShinyMsg, key: &str) -> bool {
+        let v: &serde_json::Value = &msg.data[key];
+        let prev_val = self.input().get(key).unwrap_or(&serde_json::Value::Null);
+        if v.is_null() & prev_val.is_null() {
+            return false
+        }
+        if prev_val.is_null() | (v != prev_val) {
+            self.input().insert(&key, v.clone());
+            return true
+        }
+        return false
+    }
+}
